f90f9c6f510b422af2fd7582f37e88e6641ad1d9
Vedang <vedang.wappnet@gmail.com>
2025-05-20 17:58:24 +0530

    installed packages

---
+++ evofest/app/api/auth/register/route.ts
@@ -0,0 +1,48 @@
+// /pages/api/register.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { registerSchema } from "@/lib/validations/auth";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+    const result = registerSchema.safeParse(req.body);
+
+    if (!result.success) {
+        return res.status(400).json({ errors: result.error.flatten().fieldErrors });
+    }
+
+    const { username, email, password, role } = result.data;
+
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (existingUser) {
+            return res.status(409).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+                role
+            },
+        });
+
+        return res.status(201).json({ message: "User registered successfully"});
+    } catch (error) {
+        console.error("Registration Error:", error);
+        return res.status(500).json({ message: "Failed to register" });
+    }
+}
+
