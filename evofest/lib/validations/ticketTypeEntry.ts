ac7c95ce5b85037d85bd778405e10cc73277770b
Vedang <vedang.wappnet@gmail.com>
2025-05-21 15:52:55 +0530

    added ticketTypeEntrySchema validation

---
+++ evofest/lib/validations/ticketTypeEntry.ts
@@ -0,0 +1,13 @@
+import { Princess_Sofia } from 'next/font/google';
+import { z } from 'zod';
+
+const TicketTypeEnum = z.enum([
+  'GENERAL', 'VIP', 'FIRST50', 'FIRST100'
+]);
+
+export const ticketTypeEntrySchema = z.object({
+    eventId: z.string().min(1, "EventId is required"),
+    type: TicketTypeEnum,
+    price: z.number().int().positive("Price must be a positive integer"),
+    quantity: z.number().int().positive("QUntity must be a positive integer")
+});
