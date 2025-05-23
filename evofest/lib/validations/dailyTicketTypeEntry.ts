3da627212ccab53303d121c809bbf147a592fe71
Vedang <vedang.wappnet@gmail.com>
2025-05-23 11:15:40 +0530

    changes made in validation for dailyTicketType

--- evofest/lib/validations/dailyTicketTypeEntry.ts
+++ evofest/lib/validations/dailyTicketTypeEntry.ts
@@ -4,8 +4,8 @@ const TicketTypeEnum = z.enum([
   'GENERAL', 'VIP', 'FIRST50', 'FIRST100'
 ]);
 
-export const ticketTypeEntrySchema = z.object({
-    eventId: z.string().min(1, "EventId is required"),
+export const dailyTicketTypeEntrySchema = z.object({
+    eventScheduleId: z.string().min(1, "EventScheduleId is required"),
     type: TicketTypeEnum,
     price: z.number().int().positive("Price must be a positive integer"),
     quantity: z.number().int().positive("QUntity must be a positive integer")
--- evofest/lib/validations/eventSchedule.ts
+++ evofest/lib/validations/eventSchedule.ts
@@ -10,7 +10,8 @@ export const EventScheduleSchema = z.object({
     eventId: z.string().min(1, "EventId is required"),
     date: dateSchema,
     startTime: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/),
-    endTime: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+    endTime: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/),
+    capacity: z.number().positive('Capacity must be a positive number')
 }).refine((data) => data.startTime < data.endTime, {
     message: "End Time must be after start time"
 });
